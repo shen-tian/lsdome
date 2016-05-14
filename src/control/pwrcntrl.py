b3d7dbac024440da72fe145626b0c6f278d4977d
Shen Tian <biozinc@gmail.com>
2016-05-14 16:42:39 +0200

    Improved power controller code

--- src/control/pwrcntrl.py
+++ src/control/pwrcntrl.py
@@ -11,32 +11,28 @@ import sys
 import threading
 import SocketServer
 from time import sleep
+import time
 import datetime
 import json
 import PyNUT
+import csv
+import os
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(name)s: %(message)s',
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     )
 
-# Hard coded, of course... 
-nut = PyNUT.PyNUTClient( host="192.168.1.116", login="monuser", password="password")
-exit = False
+upsData = {}
 
 def getPowerData():
-    global nut
-    result = nut.GetUPSVars( "openups" )
-    status = result["ups.status"]
-    charge = result["battery.charge"]
-    current = float(result["output.current"])
-    voltage = float(result["output.voltage"])
     
     powerData = {
-        "ups.status" : status, 
-        "batt.charge" : charge,
-        "output.power" : current * voltage}
+        "ups.status" : upsData['ups.status'], 
+        "batt.charge" : upsData['battery.charge'],
+        "output.power" : float(upsData['output.current']) * float(upsData['output.voltage'])}
     return json.dumps(powerData)
-
+    #return "meh"
+    
 class EchoRequestHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
@@ -48,15 +44,47 @@ class EchoRequestHandler(SocketServer.BaseRequestHandler):
         finally:
             logging.info('Closing')
             return
+            
+def pollUps():
+    nut = PyNUT.PyNUTClient( host="192.168.1.125", login="monuser", password="password")
+    ofile = open(os.path.expanduser('~') + '/powerlog.csv', "ab+")
+    writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    while 1:
+        result = nut.GetUPSVars( "openups" )
+        global upsData
+        paramList = {'ups.status', 'battery.charge', 'battery.current', 'battery.voltage',
+            'battery.temperature', 'input.current', 'input.voltage', 'output.current',
+            'output.voltage'}
+        outputList = list()
+        outputList.append(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(time.time())))
+        for param in paramList:
+            upsData[param] = result[param]
+            outputList.append(result[param])
+        writer.writerow(outputList)
+        sleep(1.)
+    return    
 
 
 if __name__ == '__main__':
     import socket
     import threading
     
-
+    d = threading.Thread(target=pollUps)
+    d.setDaemon(True)
+    d.start()
+    
+    logging.info('Started polling thread')
+    
     address = ('localhost', 5204) # let the kernel give us a port
     server = SocketServer.TCPServer(address, EchoRequestHandler)
-    ip, port = server.server_address # find out what port we were given
+    #ip, port = server.server_address # find out what port we were given
 
-    server.serve_forever()
+    d2 = threading.Thread(target=server.serve_forever)
+    d2.setDaemon(True)
+    d2.start()
+    
+    logging.info("Both threads started. Press any key to exit")
+    
+    x = raw_input('Enter stuff to exit')
+
+    #server.serve_forever()
