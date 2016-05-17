cd71ce44d9bac2d3448d4fec14c251405a420f8b
Shen Tian <biozinc@gmail.com>
2016-05-17 20:58:14 +0200

    trying to set up unit testing and CI

--- src/control/pwrcntrl.py
+++ src/control/pwrcntrl.py
@@ -46,8 +46,13 @@ class EchoRequestHandler(SocketServer.BaseRequestHandler):
 def poll_ups(nut_server):
     nut_login = "monuser"
     nut_password = "password"
-
     logfile = 'powerlog.csv'
+
+    # polling and logging frequency. poll_freq < log_freq
+    poll_freq = 1.
+    log_freq = 30.
+
+    last_log_time = 0
 
     logging.info('Connecting to nut-server at %s', nut_server)
 
@@ -56,24 +61,30 @@ def poll_ups(nut_server):
 
     writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     while 1:
+        time_now = time.time()
         result = nut.GetUPSVars("openups")
         global upsData
         param_list = ['ups.status', 'battery.charge', 'battery.current', 'battery.voltage',
                       'battery.temperature', 'input.current', 'input.voltage', 'output.current',
                       'output.voltage']
         output_list = list()
-        output_list.append(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(time.time())))
+        output_list.append(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(time_now)))
 
         for param in param_list:
             upsData[param] = result[param]
             output_list.append(result[param])
-        writer.writerow(output_list)
-        time.sleep(30.)
+
+        if (time_now - last_log_time) > log_freq:
+            logging.debug('time to log')
+            writer.writerow(output_list)
+            last_log_time = time_now
+
+        time.sleep(poll_freq)
     return
 
 
 def usage():
-    print 'oops'
+    print 'pwrcntrl.py -h nut_hostname'
     return
 
 
@@ -82,7 +93,7 @@ def main(argv):
     nut_server = '127.0.0.1'
 
     try:
-        opts, args = getopt.getopt(argv, 'h:u:p', ['host=', 'user=', 'pass='])
+        opts, args = getopt.getopt(argv, 'h:u:p', ['host=', 'user=', 'password='])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
