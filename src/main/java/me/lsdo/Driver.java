9e3972d637431b9118fe350fdcdbb342cc5672e1
Shen Tian <biozinc@gmail.com>
2016-09-17 14:41:07 +0200

    PixelFlock migrated away from PDE format

--- src/main/java/me/lsdo/Driver.java
+++ src/main/java/me/lsdo/Driver.java
@@ -10,11 +10,11 @@ public class Driver
     public static final int FPS_CAP = 30;
 
     public static void main(String[] args){
+        if (args.length > 0 && args[0].equals("pixelflock"))
+            PixelFlock.main(new String[]{"me.lsdo.ab16.PixelFlock"});
+        else
+            headless(args);
         //ParticleFFT.main(new String[]{"me.lsdo.ab16.ParticleFFT"});
-
-        headless(args);
-
-
     }
 
     public static void headless(String[] args) {
