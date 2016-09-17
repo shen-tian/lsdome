483342e098e5ea2552682bf78de8e354081208ba
Shen Tian <biozinc@gmail.com>
2016-09-17 11:37:39 +0200

    Drivers can now autocycle

--- src/main/java/me/lsdo/Driver.java
+++ src/main/java/me/lsdo/Driver.java
@@ -3,6 +3,8 @@ package me.lsdo;
 import me.lsdo.ab16.*;
 import me.lsdo.processing.*;
 
+import java.util.Random;
+
 public class Driver
 {
     public static void main(String[] args){
@@ -15,38 +17,64 @@ public class Driver
 
     public static void headless(String[] args) {
 
-        long start = System.currentTimeMillis();
-
         Dome dome = new Dome(6);
         OPC opc = new OPC();
 
-        DomeAnimation animation;
         if (args.length > 0) {
-            if (args[0].equals("rings")) {
-                System.out.println("Starting rings");
-                animation = new Rings(dome, opc);
-            } else if (args[0].equals("cloud")) {
-                System.out.println("Starting cloud");
-                animation = new Cloud(dome, opc);
-            } else if (args[0].equals("kaleidoscope")) {
-                System.out.println("Starting kaleidoscope");
-                animation = new Kaleidoscope(dome, opc);
-            } else if (args[0].equals("twinkle")) {
-                System.out.println("Starting twinkle");
-                animation = new Twinkle(dome, opc);
-            } else if (args[0].equals("pixeltest")) {
-                System.out.println("Starting pixeltest");
-                animation = new PixelTest(dome, opc);
-            } else if (args[0].equals("gridtest")) {
-                System.out.println("Starting gridtest");
-                animation = new GridTest(dome, opc);
-            } else
-                animation = new Rings(dome, opc);
+            for (int i = 0; i < 5; i++)
+                RunAnimation(dome, opc, args[0], 0);
+        }
+        else{
+            Random random = new Random(0);
+            String[] animations = new String[] {
+                    "rings",
+                    "cloud",
+                    "kaleidoscope",
+                    "twinkle",
+                    "pixeltest"};
+            while (true)
+            {
+                int dice = random.nextInt(animations.length);
+                System.out.println(dice);
+                RunAnimation(dome, opc, animations[dice], 60);
+            }
+        }
+    }
+
+    private static void RunAnimation(Dome dome, OPC opc, String name, int duration)
+    {
+        DomeAnimation animation;
+        if (name.equals("rings")) {
+            System.out.println("Starting rings");
+            animation = new Rings(dome, opc);
+        } else if (name.equals("cloud")) {
+            System.out.println("Starting cloud");
+            animation = new Cloud(dome, opc);
+        } else if (name.equals("kaleidoscope")) {
+            System.out.println("Starting kaleidoscope");
+            animation = new Kaleidoscope(dome, opc);
+        } else if (name.equals("twinkle")) {
+            System.out.println("Starting twinkle");
+            animation = new Twinkle(dome, opc);
+        } else if (name.equals("pixeltest")) {
+            System.out.println("Starting pixeltest");
+            animation = new PixelTest(dome, opc);
+        } else if (name.equals("gridtest")) {
+            System.out.println("Starting gridtest");
+            animation = new GridTest(dome, opc);
         } else
             animation = new Rings(dome, opc);
 
-        while (true) {
-            double t = (System.currentTimeMillis() - start) / 1000d;
+        RunAnimation(animation, duration);
+    }
+
+    private static void RunAnimation(DomeAnimation animation, int duration)
+    {
+        long start = System.currentTimeMillis();
+        double t = 0;
+        while (t < duration || duration == 0)
+        {
+            t = (System.currentTimeMillis() - start) / 1000d;
             animation.draw(t);
         }
     }
