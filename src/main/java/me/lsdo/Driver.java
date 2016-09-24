643154cb7921ae84bd78fe7590e177248a12a1e1
Shen Tian <biozinc@gmail.com>
2016-09-24 17:10:17 +0200

    Still can't get P3D/OpenGL stuff to work

--- src/main/java/me/lsdo/Driver.java
+++ src/main/java/me/lsdo/Driver.java
@@ -12,9 +12,10 @@ public class Driver
     public static void main(String[] args){
         if (args.length > 0 && args[0].equals("pixelflock"))
             PixelFlock.main(new String[]{"me.lsdo.ab16.PixelFlock"});
+        else if (args.length > 0 && args[0].equals("particlefft"))
+            ParticleFFT.main(new String[]{"me.lsdo.ab16.ParticleFFT"});
         else
             headless(args);
-        //ParticleFFT.main(new String[]{"me.lsdo.ab16.ParticleFFT"});
     }
 
     public static void headless(String[] args) {
